1fd1e7151ba3000f6a01966403cef3e62a04924d
resumeproject <killerhyper89@gmail.com>
2025-02-28 11:12:47 +0530

    changes

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!d075#t*uctx#bfmwem&&oqk&u65*mlxaoholi1c70#fe-as)('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*','vercel.app']
 
@@ -66,7 +66,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                "whitenoise.middleware.WhiteNoiseMiddleware",
+               
             ],
         },
     },
